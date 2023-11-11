033a3366973f66d26465c83f4da9e8d5089448d6
Владислав Ткаченко <Tkachenko.V.A@nmu.one>
2023-11-11 13:15:55 +0200

    Створив контроллер для замовленнь

---
+++ PhoneStore/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.Language.Intermediate;
+using PhoneStore.DAL.Repository.IRepository;
+using PhoneStore.Entities;
+using PhoneStore.Services;
+using SQLitePCL;
+using Stripe.Tax;
+using System.Security.Claims;
+
+namespace PhoneStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region APICALLS
+        public IActionResult GetAll()
+        {
+            var claim = (ClaimsIdentity)User.Identity;
+            var userId = claim.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            List<OrderHeader> orderHeadersLists;
+
+
+            _unitOfWork.ApplicationUser.GetAsync(u => u.Id == userId);
+
+            if (User.IsInRole(SD.Role_Admin))
+            {
+                orderHeadersLists = _unitOfWork.OrderHeader.GetAll().ToList();
+            }
+            else
+            {
+                orderHeadersLists = _unitOfWork.OrderHeader.GetAll(u => u.ApplicationUserId == userId, IncludedProperties: "Product").ToList();
+
+            }
+
+            return Json(new { data = orderHeadersLists });
+        }
+
+        #endregion
+
+
+    }
+}
